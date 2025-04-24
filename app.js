4939b41a5e9a0b8021b72087271f3cccc4135465
Flor <mariaflorenciaperezd@gmail.com>
2025-04-24 20:39:58 +0200

    back terminado

--- app.js
+++ app.js
@@ -3,3 +3,34 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const urlUsers = `https://api-books-ac3j.onrender.com/users`;
+const urlBooks = `https://api-books-ac3j.onrender.com/books`;
+
+app.use(cors());
+app.use(express.json());    
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/users', async (req, res) => {
+  try {
+    const response = await axios.get(urlUsers);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching users');
+  }
+});
+
+app.get('/books', async (req, res) => {
+  try {
+    const response = await axios.get(urlBooks);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching books');
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
