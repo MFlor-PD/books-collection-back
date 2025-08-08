565f78e21d8efc3eef0fb9c79abd10f630636d5f
Flor <mariaflorenciaperezd@gmail.com>
2025-08-08 08:29:10 +0200

    gitisgonre added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());    
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) =>{
+  res.send('Pagina Principal')
+}),
+
 app.get('/users', async (req, res) => {
   try {
     const response = await axios.get(urlUsers);
